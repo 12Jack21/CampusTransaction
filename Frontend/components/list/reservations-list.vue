92c17a5b4829a3cbe69f32804ea3ac166eb3a8a7
marine0927 <45658966+marine0927@users.noreply.github.com>
2020-05-22 09:16:02 +0800

    预约列表

---
+++ Frontend/components/list/reservations-list.vue
@@ -0,0 +1,56 @@
+<template>
+	<view>
+		<block v-for="(item) in list_data" :key="item.id">
+
+			<view class="zaiui-goods-list-box" @tap="listTap(item.id)">
+				
+				<view class="cu-avatar radius" :style="[{backgroundImage:'url('+ item.img +')'}]"/>
+				<view class="goods-info-view">
+					<view class="text-cut-2 text-black">{{item.title}}</view>
+					<view class="goods-info-tools">
+						<text class="text-price text-red text-lg">{{item.price}}</text>
+					</view>
+				</view>
+			</view>
+		</block>
+	</view>
+
+	
+</template>
+
+<script>
+	import barTitle from '@/components/basics/bar-title';
+	import _tool from '@/static/zaiui/util/tools.js';	//工具函数
+	
+	export default {
+		name: 'reservationList',
+		
+		props: {
+			list_data: {
+				type: Array,
+				default: () => {
+					return []
+				}
+			},
+		},
+		
+		methods: {
+			listTap(id) {
+				this.$emit('listTap',id)
+			},
+		}
+
+	}
+</script>
+
+
+
+<style lang="scss">
+	/* #ifdef APP-PLUS */
+		@import "../../static/colorui/main.css";
+		@import "../../static/colorui/icon.css";
+		@import "../../static/zaiui/style/app.scss";
+	/* #endif */
+	@import "../../static/zaiui/style/footmark.scss";
+</style>
+
