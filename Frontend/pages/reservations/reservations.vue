885d8151551cfddf211acc95e13ce6936123c2bc
marine0927 <45658966+marine0927@users.noreply.github.com>
2020-05-25 01:01:25 +0800

    预约列表

--- Frontend/pages/reservations/reservations.vue
+++ Frontend/pages/reservations/reservations.vue
@@ -2,78 +2,82 @@
 	<view>
 		<!--标题栏-->
 		<bar-title bgColor="bg-white" isBack @rightTap="rightTap">
-			<block slot="content">我的足迹</block>
-			<block slot="right" v-if="goods_checked">完成</block>
-			<block slot="right" v-else>编辑</block>
+			<block slot="content">我的商品</block>
 		</bar-title>
 		
 		<!--足迹的tab-->
-		<view class="bg-white zaiui-nav-view">
-			<scroll-view scroll-x class="nav z" scroll-with-animation :scroll-left="headTab.scrollLeft">
-				<block v-for="(item,index) in headTab.list" :key="index">
-					<view class="cu-item" :class="index==headTab.TabCur?'select':''" @tap="tabSelect" :data-id="index">
-						<view :class="index==headTab.TabCur?'text-black':''">{{item}}</view>
-						<view class="tab-dot bg-red"/>
-					</view>
-				</block>
-			</scroll-view>
-		</view>
-		
-		<checkbox-group class="block" @change="checkboxChange">
-			<!--商品列表-->
-			<view class="bg-white zaiui-goods-list-view" :class="goods_checked?'checked':''">
-				<view class="zaiui-checkbox-view">
-					<checkbox class='round red zaiui-checked' :class="checkbox_list[0].checked?'checked':''"
-					:checked="checkbox_list[0].checked?true:false" :value="checkbox_list[0].id + ''"/>
-					<view class="text-black">今天</view>
-				</view>
-				<view class="zaiui-goods-list-box" @tap="listTap(1)">
-					<checkbox class='round red zaiui-checked' :class="checkbox_list[1].checked?'checked':''"
-					:checked="checkbox_list[1].checked?true:false" :value="checkbox_list[1].id + ''"/>
-					<view class="cu-avatar radius" :style="[{backgroundImage:'url('+ goods_img +')'}]"/>
-					<view class="goods-info-view">
-						<view class="text-cut-2 text-black">商品名称 99新 苹果 iPhoneX</view>
-						<view class="goods-info-tools">
-							<text class="text-price text-red text-lg">2999.00</text>
-							<text class="cu-tag radius line-red sm">找相似</text>
-						</view>
-						<view class="btn-right-view">
-							<button class="cu-btn bg-red round sm">确认预约</button>
-						</view>
-					</view>
 
-				</view>
-				<view class="zaiui-goods-list-box">
-					<checkbox class='round red zaiui-checked' :class="checkbox_list[2].checked?'checked':''"
-					:checked="checkbox_list[2].checked?true:false" :value="checkbox_list[2].id + ''"/>
-					<view class="cu-avatar radius" :style="[{backgroundImage:'url('+ goods_img +')'}]"/>
-					<view class="goods-info-view">
-						<view class="text-cut-2 text-black">商品名称 99新 苹果 iPhoneX 256G 银色 测试换行内容的 测试一下效果的</view>
-						<view class="goods-info-tools">
-							<text class="text-price text-red text-lg">2999.00</text>
-							<text class="cu-tag radius line-red sm">找相似</text>
-						</view>
-					</view>
+		<view class="bg-white zaiui-title-tab-box">
+
+			<view class="flex flex-wrap">
+				<view class="basis-l">
+					<scroll-view scroll-x class="nav z" scroll-with-animation :scroll-left="headTab.scrollLeft">
+						<block v-for="(item,index) in headTab.list" :key="index">
+							<view class="cu-item" :class="index==tabCur?'select':''" @tap="tabSelect" :data-id="index">
+								<view :class="index==tabCur?'text-black':''">{{item}}</view>
+								<view class="tab-dot bg-red"/>
+							</view>
+						</block>
+					</scroll-view>
 				</view>
 			</view>
+		</view>
+		
+		<you-scroll ref="scroll" @onPullDown="onPullDown">
+			<!--广场内容区域-->
+			<checkbox-group class="block" @change="checkboxChange" v-show="tabCur === 0">
+				<!--商品列表-->
+				<view class="bg-white zaiui-goods-list-view" >
+					<reservationList
+						:list_data="stage1.list"
+						@listTap="listTap"
+					/>
+				</view>
+				
+			</checkbox-group>
 			
-		</checkbox-group>
+			<checkbox-group class="block" @change="checkboxChange" v-show="tabCur === 1">
+				<!--商品列表-->
+				<view class="bg-white zaiui-goods-list-view" >
+					<reservationList
+						:list_data="stage2.list"
+						@listTap="listTap"
+					/>
+				</view>
+				
+			</checkbox-group>
+			
+			<checkbox-group class="block" @change="checkboxChange" v-show="tabCur === 2">
+				<!--商品列表-->
+				<view class="bg-white zaiui-goods-list-view" >
+					<reservationList
+						:list_data="stage3.list"
+						@listTap="listTap"
+					/>
+				</view>
+				
+			</checkbox-group>
+			
+			<checkbox-group class="block" @change="checkboxChange" v-show="tabCur === 3">
+				<!--商品列表-->
+				<view class="bg-white zaiui-goods-list-view" >
+					<reservationList
+						:list_data="totalList.list"
+						@listTap="listTap"
+					/>
+				</view>
+				
+			</checkbox-group>
+		
+		</you-scroll>
+		
+		
+		
+		
 		
 		<!--占位底部距离-->
 		<view class="cu-tabbar-height" v-if="goods_checked"/>
 		
-		<!--底部操作-->
-		<view class="bg-white zaiui-footer-fixed zaiui-foot-padding-bottom" v-if="goods_checked">
-			<view class="cu-bar padding-lr">
-				<view class="checked-view" @tap="tapChecked"> 
-					<checkbox class='round red sm zaiui-checked' :class="checkbox_all?'checked':''" :checked="checkbox_all"/>
-					<text class="text-black text-lg">全选</text>
-				</view>
-				<view class="btn-view">
-					<button class="cu-btn radius bg-red">删除(3)</button>
-				</view>
-			</view>
-		</view>
 		
 		<!--小程序端显示-->
 		<!-- #ifdef MP -->
@@ -89,28 +93,55 @@
 <script>
 	import barTitle from '@/components/basics/bar-title';
 	import _tool from '@/static/zaiui/util/tools.js';	//工具函数
+	import _reservations_data from '@/static/zaiui/data/reservations.js';
+	import reservationList from '@/components/list/reservations-list.vue';
+	import youScroll from '@/components/you-scroll/you-scroll.vue'
+	
+	const noticeMap = tabCur => {
+		switch(tabCur){
+			case 0:
+				return 'stage1'
+			case 1:
+				return 'stage2'
+			case 2:
+				return 'stage3'
+			case 3:
+				return 'totalList'
+			default:
+				console.log('case default');
+		}
+	}
+	
+	
 	export default {
 		components: {
-			barTitle
+			barTitle,
+			reservationList,
+			youScroll
 		},
 		data() {
 			return {
-				headTab: {TabCur: 0, scrollLeft: 0, list: []},
+				headTab: {scrollLeft: 0, list: []},
+				tabCur:0,
 				goods_img: '/static/images/home/goods/1.png',
 				goods_img_a: '/static/images/home/goods/2.png',
 				checkbox_list: [], checkbox_all: false, goods_checked: false,
-				
+				stage1: {list:[]},
+				stage2: {list:[]},
+				stage3: {list:[]},
+				totalList: {list:[]},
 				reservations: []
 			}
 		},
 		onLoad(params) {
-			this.checkbox_list = [
-				{id: 1,checked: true}, {id: 2,checked: false}, {id: 3,checked: false},
-				{id: 4,checked: false}, {id: 5,checked: false}, {id: 6,checked: false}
-			];
-			this.headTab.list = ['全部预约'];
-			
-			this.getReservations(params.accountId)
+
+			this.headTab.list = ['待确认','预约中','已完成','全部预约'];
+			this.stage1.list=_reservations_data.stage1List()
+			this.stage2.list=_reservations_data.stage2List()
+			this.stage3.list=_reservations_data.stage3List()
+			this.totalList.list=_reservations_data.totalList()
+			this.loadNotices()
+			//this.getReservations(params.accountId)
 		},
 		onReady() {
 			_tool.setBarColor(true);
@@ -134,17 +165,65 @@
 					url: '../../pages/detail/reservation?id=' + id
 				});
 			},
-			//tab菜单被点击
-			tabSelect(e) {
-				let index = e.currentTarget.dataset.id;
-				this.headTab.TabCur = index;
-				this.headTab.scrollLeft = (index - 1) * 60;
+			async loadNotices(){
+				this.loadStatus = 'loading'
+				let curNotices = this[noticeMap(this.tabCur)]
+				let pagination = {
+					pageIndex:curNotices.pageIndex,
+					pageSize: curNotices.pageSize,
+					startTime:curNotices.startTime
+				}
+				if(this.tabCur > 2){
+					await this.$api.getMyNotices(this.userId,pagination)
+						.then(({data})=>{
+							curNotices.list.push(...data.list)
+							// 取完了数据
+							if(data.pageIndex === data.pageCount)
+								curNotices.finish = true
+						})
+						.catch(()=>uni.showToast({
+							title: '我的通告获取失败',
+							icon: 'none'
+						}))
+				}
+				else
+					await this.$api.getNotices(this.tabCur, pagination)
+						.then(({data})=> {
+							// 一些没有判断 success,根据后台的 json 来决定要不要加这个判断
+							curNotices.list.push(...data.list)
+							// 取完了数据
+							if(data.pageIndex === data.pageCount)
+								curNotices.finish = true
+						})
+						.catch(() => uni.showToast({
+							title: '通告获取失败',
+							icon: 'none'
+						}))
 				
-				//滚动到顶部
-				uni.pageScrollTo({
-				    scrollTop: 0,
-				    duration: 100
-				});
+				console.log('运行到此，设置 loadingText');
+				if(curNotices.finish)
+					this.loadStatus = 'noMore'
+				else
+					this.loadStatus = 'more'
+			},
+			async onPullDown(done){
+				this[noticeMap(this.tabCur)] = iniNotice()
+				await this.loadNotices()
+				done()		
+			},
+			setReachBottom() {
+				console.log('notice 触底了')
+				if(!this[noticeMap(this.tabCur)].finish)
+					this.loadNotices()
+			},
+			tabSelect(e) {
+				let current = parseInt(e.currentTarget.dataset.id)		
+				if(this.tabCur !== current && this[noticeMap(current)].list.length === 0){
+					this[noticeMap(current)] = iniNotice()
+					this.tabCur = current
+					this.loadNotices()
+				}else
+					this.tabCur = current
 			},
 			//编辑
 			rightTap() {
@@ -173,7 +252,7 @@
 				} else {
 					this.checkbox_all = true;
 				}
-			}
+			},
 		}
 	}
 </script>
@@ -185,4 +264,25 @@
 		@import "../../static/zaiui/style/app.scss";
 	/* #endif */
 	@import "../../static/zaiui/style/footmark.scss";
+	
+
+</style>
+
+<style lang="scss" scoped>
+	
+	.zaiui-title-tab-box {
+		position: fixed;
+		width: 100%;
+		top: 40px;
+		z-index: 999999;
+		padding: calc(var(--status-bar-height) + 9.09rpx) 27.27rpx 9.09rpx 9.09rpx;
+		.flex {
+			.basis-l {
+				flex-basis: 70%;
+			}
+			.basis-s {
+				flex-basis: 30%;
+			}
+		}
+	}
 </style>
