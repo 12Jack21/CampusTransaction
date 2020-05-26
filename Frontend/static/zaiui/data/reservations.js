357279a45479e1e8e1eb954927fb318b85f4d216
marine0927 <45658966+marine0927@users.noreply.github.com>
2020-05-26 09:08:26 +0800

    Add files via upload

---
+++ Frontend/static/zaiui/data/reservations.js
@@ -0,0 +1,133 @@
+let _reservations_data = {
+	stage1List() {
+		return [
+			{
+				mold: true,
+				price: '2280',
+				title: '商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题',
+				img: '/static/images/home/goods/1.png',
+				expiredTime:'2020-10-09'
+				
+			},
+			{
+				mold: false,
+				price: '5049',
+				title: '商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题',
+				img: '/static/images/home/goods/2.png',
+				expiredTime:'2020-10-09'
+			}
+		];
+	},
+	stage2List() {
+		return [
+			{
+				mold: true,
+				price: '2980',
+				title: '商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题',
+				img: '/static/images/home/goods/3.png',
+				expiredTime:'2020-10-09'
+			},
+			{
+				mold: true,
+				price: '2280',
+				title: '商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题',
+				img: '/static/images/home/goods/4.png',
+				expiredTime:'2020-10-09'
+			},
+			{
+				mold: true,
+				price: '5049',
+				title: '商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题',
+				img: '/static/images/home/goods/5.png',
+				expiredTime:'2020-10-09'
+			}
+		];
+	},
+	stage3List() {
+		return [
+			{
+				mold: true,
+				price: '2980',
+				title: '商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题',
+				img: '/static/images/home/goods/6.png',
+				expiredTime:'2020-10-09'
+			},
+			{
+				mold: true,
+				price: '2280',
+				title: '商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题',
+				img: '/static/images/home/goods/7.png',
+				expiredTime:'2020-10-09'
+			},
+			{
+				mold: true,
+				price: '5049',
+				title: '商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题',
+				img: '/static/images/home/goods/8.png',
+				expiredTime:'2020-10-09'
+			}
+		];
+	},
+	totalList() {
+		return [
+			{
+				mold: true,
+				price: '2280',
+				title: '商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题',
+				img: '/static/images/home/goods/1.png',
+				expiredTime:'2020-10-09'
+			},
+			{
+				mold: false,
+				price: '5049',
+				title: '商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题',
+				img: '/static/images/home/goods/2.png',
+				expiredTime:'2020-10-09'
+			},
+			{
+				mold: true,
+				price: '2980',
+				title: '商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题',
+				img: '/static/images/home/goods/3.png',
+				expiredTime:'2020-10-09'
+			},
+			{
+				mold: true,
+				price: '2280',
+				title: '商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题',
+				img: '/static/images/home/goods/4.png',
+				expiredTime:'2020-10-09'
+			},
+			{
+				mold: true,
+				price: '5049',
+				title: '商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题',
+				img: '/static/images/home/goods/5.png',
+				expiredTime:'2020-10-09'
+			},
+			{
+				mold: true,
+				price: '2980',
+				title: '商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题',
+				img: '/static/images/home/goods/6.png',
+				expiredTime:'2020-10-09'
+			},
+			{
+				mold: true,
+				price: '2280',
+				title: '商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题',
+				img: '/static/images/home/goods/7.png',
+				expiredTime:'2020-10-09'
+			},
+			{
+				mold: true,
+				price: '5049',
+				title: '商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题，商品标题',
+				img: '/static/images/home/goods/8.png',
+				expiredTime:'2020-10-09'
+			}
+		];
+	},
+}
+
+export default _reservations_data;
