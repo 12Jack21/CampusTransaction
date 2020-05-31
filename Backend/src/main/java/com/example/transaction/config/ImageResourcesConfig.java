f4b6c42b5d16791b22e2ea6395c45258bb3a02b9
ZhihaoZeng <840047816@qq.com>
2020-05-31 21:19:09 +0800

    更新对应的资源路径配置

--- Backend/src/main/java/com/example/transaction/config/ImageResourcesConfig.java
+++ Backend/src/main/java/com/example/transaction/config/ImageResourcesConfig.java
@@ -2,8 +2,12 @@ package com.example.transaction.config;
 
 import com.example.transaction.util.code.ResourcePath;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 
 /**
  * @ClassName: MyWebAppConfigurator
@@ -20,9 +24,15 @@ public class ImageResourcesConfig implements WebMvcConfigurer {
 //        registry.addResourceHandler("/static/images/home/goods/**")
 //                .addResourceLocations("file:"+ ResourcePath.basePath + "/src/main/resources/static/images/home/goods/");
         /*打包后的路径*/
+//        registry.addResourceHandler("/static/images/avatar/**")
+//                .addResourceLocations("file:" + ResourcePath.basePath + "/BOOT-INF/classes/static/images/avatar/");
+//        registry.addResourceHandler("/static/images/home/goods/**")
+//                .addResourceLocations("file:" + ResourcePath.basePath + "/BOOT-INF/classes/static/images/home/goods/");
         registry.addResourceHandler("/static/images/avatar/**")
-                .addResourceLocations("file:" + ResourcePath.basePath + "/BOOT-INF/classes/static/images/avatar/");
+                .addResourceLocations("classpath:/static/images/avatar/");
         registry.addResourceHandler("/static/images/home/goods/**")
-                .addResourceLocations("file:" + ResourcePath.basePath + "/BOOT-INF/classes/static/images/home/goods/");
+                .addResourceLocations("classpath:/static/images/home/goods/");
+
+
     }
 }
