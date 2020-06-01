c09cb34559d45ec6bf251225a4afbfcdeaf614c2
12Jack21 <michealgray3367@hotmail.com>
2020-06-01 08:20:54 +0800

    add appreciation

--- Frontend/main.js
+++ Frontend/main.js
@@ -5,7 +5,10 @@ import api from '@/utils/api.js'
 import mpopup from '@/components/xuan-popup/xuan-popup.vue'
 import loading from '@/components/basics/loading.vue'
 
-// const http = new Request()
+/**
+ * 本项目使用了 UNI-APP 仿转转二手市场APP页面模板 进行开发: https://github.com/itianc/uni-turnTurn-Shop-Templet
+ * 在此感谢 @仔仔ZaiZ 提供的非常齐全的模板
+ * */
 
 Vue.config.productionTip = false
 Vue.component('mpopup',mpopup)
